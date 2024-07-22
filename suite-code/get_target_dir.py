3d8c9ff3d62d45566aa1f989870178062eaecfc5
Jean Henry <jean.henry@ansys.com>
2024-07-22 18:07:28 +0200

    fix: Revert to initial design with errors fixed

--- suite-code/get_target_dir.py
+++ suite-code/get_target_dir.py
@@ -72,7 +72,7 @@ def main(name: str):
         .replace("$(NodeName)", node_name)
         .replace("$(Configuration)", name)
     )
-    # print the result
-    print(target_dir)
+    # print the result as the definition of an environment variable
+    print("target-directory=%s" % target_dir)
     # report a message on stderr, return status not reported by scade.exe -script
     print("Command completed.", file=stderr)
--- tests/suite-code/TestGetTargetDir.bat
+++ tests/suite-code/TestGetTargetDir.bat
@@ -1,10 +1,9 @@
 @echo off
 "c:\Program Files\ANSYS Inc\v242\SCADE\SCADE\bin\SCADE.exe" -script Model/Model.etp ..\..\suite-code\get_target_dir.py "main('Suite Code')" > log.txt 2> err.txt
 if errorlevel 1 (type log.txt & exit /b 1)
-:: check for the correct completion of the command,
+:: check for the correct completion of the command
 :: when no error code is returned by scade.exe -script
 type err.txt | find "Command completed" > nul
 if errorlevel 1 (type err.txt & exit /b 1)
-for /F %%t in (log.txt) do (
-    echo target-dir=%%t
-)
+type log.txt >> env.txt
+type env.txt
