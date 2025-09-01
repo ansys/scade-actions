9491dda51ca6ec8593641f110cc9afb74e3f0f08
Ludovic O. <34745062+ansloddos@users.noreply.github.com>
2025-09-01 10:53:58 +0200

    fix: modify suite-rules/suite-metrics  actions because wrong error code returned by SCADE  (#100)

--- tests/suite-rules/TestCheckRules.bat
+++ tests/suite-rules/TestCheckRules.bat
@@ -1,33 +1,33 @@
 @echo off
 :: nominal check without error
-call ..\..\suite-rules\CheckRules.bat "c:\Program Files\ANSYS Inc\v242\SCADE" "Model/Model.etp" "RuleSuccess" false
+call ..\..\suite-rules\CheckRules.bat "c:\Program Files\ANSYS Inc\v242\SCADE" "Model/Model.etp" "RuleSuccess"
 if errorlevel 1 (
     echo unexpected checker error
 )
 @echo off
 :: nominal check with error
-call ..\..\suite-rules\CheckRules.bat "c:\Program Files\ANSYS Inc\v242\SCADE" "Model/Model.etp" "RuleFailure" false
+call ..\..\suite-rules\CheckRules.bat "c:\Program Files\ANSYS Inc\v242\SCADE" "Model/Model.etp" "RuleFailure"
 if errorlevel 1 (
     echo *checker errors detected*
 ) else (
     echo error: checker error not detected
 )
 :: check for errors
-call ..\..\suite-rules\CheckRules.bat "c:\Program Files\ANSYS Inc\v242\xSCADE" "Model/Model.etp" "RuleSuccess" false
+call ..\..\suite-rules\CheckRules.bat "c:\Program Files\ANSYS Inc\v242\xSCADE" "Model/Model.etp" "RuleSuccess"
 if errorlevel 1 (
     echo *wrong SCADE dir detected*
 ) else (
     echo error: wrong SCADE dir not detected
 )
 
-call ..\..\suite-rules\CheckRules.bat "c:\Program Files\ANSYS Inc\v242\SCADE" "Model/xModel.etp" "RuleSuccess" false
+call ..\..\suite-rules\CheckRules.bat "c:\Program Files\ANSYS Inc\v242\SCADE" "Model/xModel.etp" "RuleSuccess"
 if errorlevel 1 (
     echo *wrong project detected*
 ) else (
     echo error: wrong project not detected
 )
 
-call ..\..\suite-rules\CheckRules.bat "c:\Program Files\ANSYS Inc\v242\SCADE" "Model/Model.etp" "Unknown" false
+call ..\..\suite-rules\CheckRules.bat "c:\Program Files\ANSYS Inc\v242\SCADE" "Model/Model.etp" "Unknown"
 if errorlevel 1 (
     echo *checker error detected*
 ) else (
